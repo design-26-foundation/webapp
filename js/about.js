6470b980a77f5f9a56689c450cc6a06a52ca9bc9
Shailen <shailen@gettruck.co.za>
2019-07-13 14:23:29 +0200

    Fetching data for templating

--- js/about.js
+++ js/about.js
@@ -1,4 +1,27 @@
-const about = new Vue({
+function transformData (response) {
+  return {
+    banner: {
+      image: {
+        url: response.acf.banner_image.url,
+        text: response.acf.banner_image.alt,
+      },
+      heading: response.acf.banner_title,
+      text: response.acf.banner_text,
+    },
+    aboutBlocks: response.acf.home_block_repeater.map(function (props) {
+      return {
+        title: props.block_title,
+        content: props.block_content,
+        image: {
+          url: props.block_image.url,
+          text: props.block_image.alt,
+        }
+      }
+    })
+  }
+}
+
+var about = new Vue({
   el: '#about-app',
   data: {
     banner: {
@@ -12,3 +35,10 @@ const about = new Vue({
     aboutBlocks: [],
   },
 });
+
+axios.get(ENDPOINTS.about).then(function (res) {
+  const data = transformData(res.data);
+
+  about.banner = data.banner;
+  about.aboutBlocks = data.aboutBlocks;
+})
