424f13fb6daab72b24c28a2bf8f52e68ae18447a
Shailen <shailen@gettruck.co.za>
2019-07-13 13:47:40 +0200

    Added components to separate file

--- js/home.js
+++ js/home.js
@@ -1,24 +1,3 @@
-Vue.component('banner', {
-  template: `
-    <article>
-      <figure class="split-image">
-        <img :src="data.image.url" :alt="data.image.text" />
-      </figure>
-      <div class="cta dark-cta">
-        <h2>{{ data.heading }}</h2>
-        <p>{{ data.text }}</p>
-        <button class="button button-pink">{{ data.buttonText }}</button>
-      </div>
-    </article>
-  `,
-  props: {
-    data: {
-      type: Object,
-      required: true,
-    }
-  },
-});
-
 var home = new Vue({
   el: '#home-app',
   data: {
