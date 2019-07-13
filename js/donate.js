142b0e873e8b51f36f58d3e1bdb738cd01de71b2
Shailen <shailen@gettruck.co.za>
2019-07-13 15:12:12 +0200

    Setup donate template

--- js/donate.js
+++ js/donate.js
@@ -1,3 +1,25 @@
 const donate = new Vue({
   el: '#donate-app',
+  data: {
+    banner: {
+      image: {
+        url: '',
+        text: '',
+      },
+      heading: '',
+      text: '',
+    },
+    aboutBlocks: [
+      {
+        title: '',
+        content: '',
+        buttonText: '',
+        buttonUrl: '',
+        image: {
+          url: '',
+          text: '',
+        }
+      }
+    ],
+  },
 });
