2ac94ac36afd641e3936a2c387c4ee7fc3d92e82
Shailen <shailen@gettruck.co.za>
2019-07-13 16:45:27 +0200

    Updated footer

--- js/blog.js
+++ js/blog.js
@@ -11,6 +11,20 @@ function transformData (response) {
   }
 }
 
+function transformPostsArray (response) {
+  return response.map(function (props) {
+    return {
+      id: props.id,
+      title: props.title.rendered,
+      content: props.excerpt.rendered,
+      image: {
+        url: props.acf.image.url,
+        text: props.acf.image.text,
+      }
+    }
+  })
+}
+
 var blog = new Vue({
   el: '#blog-app',
   data: {
@@ -28,6 +42,11 @@ var blog = new Vue({
 
 axios.get(ENDPOINTS.blog).then(function (res) {
   var data = transformData(res.data);
-
   blog.banner = data.banner;
+
+  axios.get(POSTS_ENDPOINT).then(function (res) {
+    const data = transformPostsArray(res.data);
+
+    blog.blogPosts = data;
+  });
 });
